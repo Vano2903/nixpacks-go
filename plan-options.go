fb675f06097300929f59b17dbb3d6e4e54532ed0
vano2903 <davidevanoncini2003@gmail.com>
2023-03-15 16:31:46 +0100

    fix: remove fmt prints and fixes command args

--- plan-options.go
+++ plan-options.go
@@ -2,7 +2,6 @@ package nixpacks
 
 import (
 	"errors"
-	"fmt"
 )
 
 type PlanOptions struct {
@@ -34,48 +33,49 @@ func (p PlanOptions) Validate() error {
 
 }
 
-func (p PlanOptions) ToArgs() []string {
+func (o PlanOptions) ToArgs() []string {
 	var args []string
 
-	if p.InstallCommand != "" {
-		args = append(args, fmt.Sprintf("--install-cmd %q", p.InstallCommand))
+	if o.InstallCommand != "" {
+		args = append(args, "--install-cmd "+o.InstallCommand)
 	}
 
-	if p.BuildCommand != "" {
-		args = append(args, fmt.Sprintf("--build-cmd %q", p.BuildCommand))
+	if o.BuildCommand != "" {
+		args = append(args, "--build-cmd "+o.BuildCommand)
 	}
 
-	if p.StartCommand != "" {
-		args = append(args, fmt.Sprintf("--start-cmd %q", p.StartCommand))
+	if o.StartCommand != "" {
+		args = append(args, "--start-cmd "+o.StartCommand)
 	}
 
-	if len(p.NixPackages) != 0 {
-		for _, pkg := range p.NixPackages {
-			args = append(args, fmt.Sprintf("--pkgs %q", pkg))
+	if len(o.NixPackages) != 0 {
+		for _, pkg := range o.NixPackages {
+			args = append(args, "--pkgs "+pkg)
 		}
 	}
 
-	if len(p.AptPackages) != 0 {
-		for _, pkg := range p.AptPackages {
-			args = append(args, fmt.Sprintf("--apt %q", pkg))
+	if len(o.AptPackages) != 0 {
+		for _, pkg := range o.AptPackages {
+			args = append(args, "--apt "+pkg)
 		}
 	}
 
-	if len(p.NixLibraries) != 0 {
-		for _, lib := range p.NixLibraries {
-			args = append(args, fmt.Sprintf("--libs %q", lib))
+	if len(o.NixLibraries) != 0 {
+		for _, lib := range o.NixLibraries {
+			args = append(args, "--libs "+lib)
 		}
 	}
 
-	if len(p.Envs) != 0 {
-		for _, env := range p.Envs {
-			args = append(args, fmt.Sprintf("--env %q", env.Key+"="+env.Value))
+	if len(o.Envs) != 0 {
+		for _, env := range o.Envs {
+			args = append(args, "--env "+env.Key+"="+env.Value)
 		}
 	}
 
-	if p.Config != "" {
-		args = append(args, fmt.Sprintf("--config %q", p.Config))
+	if o.Config != "" {
+		args = append(args, "--config "+o.Config)
 	}
 
+	args = append(args, "--verbose")
 	return args
 }
--- plan.go
+++ plan.go
@@ -2,7 +2,6 @@ package nixpacks
 
 import (
 	"context"
-	"fmt"
 	"os/exec"
 )
 
@@ -15,9 +14,6 @@ func (n Nixpacks) Plan(ctx context.Context, opt PlanOptions) (*PlanCmd, error) {
 		return nil, err
 	}
 
-	fmt.Println("Building: ", opt.Path)
-	fmt.Println("args:", opt.ToArgs())
-
 	cmd := exec.CommandContext(ctx, n.commandPath, PlanCommand, opt.Path)
 	cmd.Args = append(cmd.Args, opt.ToArgs()...)
 
@@ -28,19 +24,14 @@ func (n Nixpacks) Plan(ctx context.Context, opt PlanOptions) (*PlanCmd, error) {
 
 func (c *PlanCmd) Result() (PlanOutput, error) {
 	b := PlanOutput{}
-	fmt.Println(c.cmd.String())
 	out, err := c.cmd.CombinedOutput()
 	if err != nil {
-		fmt.Println(err)
 		if err.Error() == "signal: killed" {
 			return b, err
 		}
 	}
 	b.Response = out
 	b.Parse()
-	b.Response = nil //! remove, only for debugging
-	fmt.Printf("out: %+v\n", b)
-	fmt.Println()
 	return b, err
 
 }
